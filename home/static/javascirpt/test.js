617e7ccfde55a5c761b679463cf83d5219a98058
kuc11 <rladmlcks3124@naver.com>
2024-01-16 17:30:13 +0900

    0116

--- home/static/javascirpt/test.js
+++ home/static/javascirpt/test.js
@@ -1,33 +1,25 @@
-class FAQ {
-    constructor(target) {
-        this.target = typeof target === 'string' ? document.querySelector(target) : target;
-        this.faqBody = this.target.querySelector('.faq_body');
-        this.maxHeight = `${this.faqBody.scrollHeight}px`;
-        this.toggleBtn = this.target.querySelector('button');
-        this.opened = false;
-
-        this.init();
-    }
-
-    init() {
-        this.toggleBtn.addEventListener('click', () => {
-            this.toggleButton();
-        });
-    }
-
-    toggleButton() {
-        const buttonIcon = this.toggleBtn.querySelector('i');
-
-        if (this.opened) {
-            buttonIcon.classList.remove('fa-square-minus');
-            buttonIcon.classList.add('fa-square-plus');
-            this.faqBody.style.maxHeight = '0';
-            this.opened = false;
-        } else {
-            buttonIcon.classList.remove('fa-square-plus');
-            buttonIcon.classList.add('fa-square-minus');
-            this.faqBody.style.maxHeight = this.maxHeight;
-            this.opened = true;
-        }
-    }
-}
+window.onload = () => {
+    // panel-faq-container
+    const panelFaqContainer = document.querySelectorAll(".panel-faq-container"); // NodeList 객체
+    
+    // panel-faq-answer
+    let panelFaqAnswer = document.querySelectorAll(".panel-faq-answer");
+  
+    // btn-all-close
+    const btnAllClose = document.querySelector("#btn-all-close");
+    
+    // 반복문 순회하면서 해당 FAQ제목 클릭시 콜백 처리
+    for( let i=0; i < panelFaqContainer.length; i++ ) {
+      panelFaqContainer[i].addEventListener('click', function() { // 클릭시 처리할 일
+        // FAQ 제목 클릭시 -> 본문이 보이게끔 -> active 클래스 추가
+        panelFaqAnswer[i].classList.toggle('active');
+      });
+    };
+    
+    btnAllClose.addEventListener('click', function() {
+      // 버튼 클릭시 처리할 일  
+      for(let i=0; i < panelFaqAnswer.length; i++) {
+          panelFaqAnswer[i].classList.remove('active');
+      };
+    });
+  }
